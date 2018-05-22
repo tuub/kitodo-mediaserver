760450de828191aa2d89407a78aac43faaef8de7
Per Broman <broman@tu-berlin.de>
2018-05-22 13:24:51 +0200

    [#46] importer utils getting a work from the hotfolder and moving it to an import-in-progress folder

---
+++ kitodo-mediaserver-importer/src/main/java/org/kitodo/mediaserver/importer/config/ImporterConfiguration.java
@@ -0,0 +1,33 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+package org.kitodo.mediaserver.importer.config;
+
+import org.kitodo.mediaserver.importer.util.ImporterUtils;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * Spring configuration of the importer module.
+ */
+@Configuration
+@EnableJpaRepositories("org.kitodo.mediaserver.core.db.repositories")
+@EntityScan("org.kitodo.mediaserver.core.db.entities")
+public class ImporterConfiguration {
+
+    @Bean
+    public ImporterUtils importerUtils() {
+        return new ImporterUtils();
+    }
+
+}
--- kitodo-mediaserver-importer/src/main/java/org/kitodo/mediaserver/importer/control/ImporterFlowControl.java
+++ kitodo-mediaserver-importer/src/main/java/org/kitodo/mediaserver/importer/control/ImporterFlowControl.java
@@ -1,7 +1,21 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 package org.kitodo.mediaserver.importer.control;
 
+import java.io.File;
+import org.kitodo.mediaserver.importer.util.ImporterUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,6 +27,13 @@ import org.springframework.stereotype.Component;
 public class ImporterFlowControl implements CommandLineRunner {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ImporterFlowControl.class);
+
+    private ImporterUtils importerUtils;
+
+    @Autowired
+    public void setImporterUtils(ImporterUtils importerUtils) {
+        this.importerUtils = importerUtils;
+    }
 
     /**
      * Runs the importer algorithm.
@@ -36,56 +57,58 @@ public class ImporterFlowControl implements CommandLineRunner {
     /* @Scheduled(fixedRate = 1000) */
     private void importScheduled() throws Exception {
 
-        /*
-            A. Create a list of works to import.
+        // Get a work from the hotfolder and move it to the import-in-progress-folder. Make sure, this set of files is
+        // in a subdirectory named as the XML file with the mets-mods-data.
 
-            B. For each item in this list:
+        File workDir;
 
-            * Make an (atomic) move of this set into the import-in-progress-folder. Make sure, this set of files is in a subdirectory named as the XML file with the mets-mods-data.
+        while ((workDir = importerUtils.getWorkPackage()) != null) {
 
-            * Validate the data and the set of files (see below).
+            System.out.println("Work to import: " + workDir.getName());
 
-            * Read the work data from the mets-mods file.
+            // * Validate the data and the set of files (see below).
+            //
+            // * Read the work data from the mets-mods file.
+            //
+            // * Check in the database if this work of any of its identifiers is already present.
+            //
+            // * If the work is already present, it should be replaced.
+            //
+            // * Files created and cached by the fileserver must be deleted.
+            //
+            // * If an identifier is already present and associated with another work, the import job should be exited
+            //   with an error.
+            //
+            // * If the work is already present, check if it is enabled (save the result).
+            //
+            // * If applicable, move old work files to a temporary folder under the import folder.
+            //
+            // * If applicable, delete old cached files.
+            //
+            // * Move work files to the production root.
+            //
+            // * Perform all defined direct import actions (doi registration…).
+            //
+            // * Order all defined asynchronous import actions (creation of additional files…).
+            //
+            // * Insert the work data into the database, updating if old data present. If the work already was present
+            //   and disabled, set enabled to false.
+            //
+            // * Delete temporary files.
+            //
+            // * Trigger indexing in presentation system (configurable).
 
-            * Check in the database if this work of any of its identifiers is already present.
-
-            * If the work is already present, it should be replaced.
-
-            * Files created and cached by the fileserver must be deleted.
-
-            * If an identifier is already present and associated with another work, the import job should be exited with an error.
-
-            * If the work is already present, check if it is enabled (save the result).
-
-            * If applicable, move old work files to a temporary folder under the import folder.
-
-            * If applicable, delete old cached files.
-
-            * Move work files to the production root.
-
-            * Perform all defined direct import actions (doi registration…).
-
-            * Order all defined asynchronous import actions (creation of additional files…).
-
-            * Insert the work data into the database, updating if old data present. If the work already was present and disabled, set enabled to false.
-
-            * Delete temporary files.
-
-            * Trigger indexing in presentation system (configurable).
-
-         */
+        }
 
 
-        /*
-            If a severe error occures during the process, the following steps must be executed:
-
-            * Move work folder to the error folder.
-
-            * If there were old data for this work present:
-
-            * Move old work files from the temporary folder back to the original folder.
-
-            * If the error occured writing to the database, rollback (of course).
-         */
+        // If a severe error occures during the process, the following steps must be executed:
+        //
+        // * Move work folder to the error folder.
+        //
+        // * If there were old data for this work present:
+        //
+        // * Move old work files from the temporary folder back to the original folder.
+        //
+        // * If the error occured writing to the database, rollback (of course).
     }
 }
