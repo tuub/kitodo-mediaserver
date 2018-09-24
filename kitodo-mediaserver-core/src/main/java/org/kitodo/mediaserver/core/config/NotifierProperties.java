57d4db1efa161c893185f1281285ab3df751a664
Fab Ian Fuerste <2682391+johlton@users.noreply.github.com>
2018-09-24 14:34:10 +0200

    [#87] Notifier feature. (#154)

---
+++ kitodo-mediaserver-core/src/main/java/org/kitodo/mediaserver/core/config/NotifierProperties.java
@@ -0,0 +1,42 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+package org.kitodo.mediaserver.core.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Properties class for the notifier configuration.
+ */
+@Configuration
+@ConfigurationProperties(prefix = "notifier")
+public class NotifierProperties {
+
+    private String emailFrom;
+    private String timestampFormat;
+
+    public String getEmailFrom() {
+        return emailFrom;
+    }
+
+    public void setEmailFrom(String emailFrom) {
+        this.emailFrom = emailFrom;
+    }
+
+    public String getTimestampFormat() {
+        return timestampFormat;
+    }
+
+    public void setTimestampFormat(String timestampFormat) {
+        this.timestampFormat = timestampFormat;
+    }
+}
