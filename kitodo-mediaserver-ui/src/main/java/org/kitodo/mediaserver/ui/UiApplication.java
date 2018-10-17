386c0b446c6b2304c784b0508b867a65a8c20983
Michael Richter <mr@osor.de>
2018-10-17 13:21:16 +0200

    [#189] Warn if local config file is missing (#190)

--- kitodo-mediaserver-ui/src/main/java/org/kitodo/mediaserver/ui/UiApplication.java
+++ kitodo-mediaserver-ui/src/main/java/org/kitodo/mediaserver/ui/UiApplication.java
@@ -11,15 +11,30 @@
 
 package org.kitodo.mediaserver.ui;
 
+import org.kitodo.mediaserver.core.util.MediaServerUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.event.EventListener;
 
 /**
  * UI application starter.
  */
 @SpringBootApplication
 public class UiApplication extends SpringBootServletInitializer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UiApplication.class);
+
+    private MediaServerUtils mediaServerUtils;
+
+    @Autowired
+    public void setMediaServerUtils(MediaServerUtils mediaServerUtils) {
+        this.mediaServerUtils = mediaServerUtils;
+    }
 
     /**
      * Starts the UI application when deployed as war in a separate servlet container.
@@ -49,4 +64,17 @@ public class UiApplication extends SpringBootServletInitializer {
                         + "application,"
                         + "dev");
     }
+
+    /**
+     * Run after application start.
+     *
+     * @param event ApplicationReadyEvent
+     */
+    @EventListener
+    public void onApplicationReady(ApplicationReadyEvent event) {
+        if (!mediaServerUtils.isLocalAppConfigLoaded()) {
+            LOGGER.warn("No local configuration file loaded (local.yml) or the file is empty. This might cause unexpected problems.");
+        }
+    }
+
 }
