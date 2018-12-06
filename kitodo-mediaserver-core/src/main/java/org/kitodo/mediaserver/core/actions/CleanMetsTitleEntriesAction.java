76c81160c730d439a4ab7c5a2e6ae884240be689
Per Broman <broman@tu-berlin.de>
2018-12-06 15:24:06 +0100

    [#222] action cleaning mets titel entries

---
+++ kitodo-mediaserver-core/src/main/java/org/kitodo/mediaserver/core/actions/CleanMetsTitleEntriesAction.java
@@ -0,0 +1,62 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+package org.kitodo.mediaserver.core.actions;
+
+import java.io.File;
+import java.util.AbstractMap;
+import java.util.Map;
+import org.kitodo.mediaserver.core.api.IAction;
+import org.kitodo.mediaserver.core.api.IMetsTransformer;
+import org.kitodo.mediaserver.core.db.entities.Work;
+import org.kitodo.mediaserver.core.util.MediaServerUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Action cleaning title entries in the mets/mods file using a regular expression with placeholder.
+ */
+public class CleanMetsTitleEntriesAction implements IAction {
+
+    private MediaServerUtils mediaServerUtils;
+    private IMetsTransformer metsTransformer;
+
+    @Autowired
+    public void setMediaServerUtils(MediaServerUtils mediaServerUtils) {
+        this.mediaServerUtils = mediaServerUtils;
+    }
+
+    public void setMetsTransformer(IMetsTransformer metsTransformer) {
+        this.metsTransformer = metsTransformer;
+    }
+
+    /**
+     * Cleans title entries in the mets file.
+     *
+     * <p>
+     * This action requires a parameter "pattern", e.g. "<<(.*?)>>".
+     *
+     * @param work      a work entity
+     * @param parameter a map of parameter
+     * @return null
+     * @throws Exception by fatal errors
+     */
+    @Override
+    public Object perform(Work work, Map<String, String> parameter) throws Exception {
+
+        mediaServerUtils.checkForRequiredParameter(parameter, "pattern");
+
+        File metsFile = mediaServerUtils.getMetsFileForWork(work);
+
+        metsTransformer.transform(metsFile, metsFile, new AbstractMap.SimpleEntry<>("pattern", parameter.get("pattern")));
+
+        return null;
+    }
+}
---
+++ kitodo-mediaserver-core/src/main/resources/xslt/cleanMetsTitleEntries.xsl
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="2.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:mets="http://www.loc.gov/METS/"
+                xmlns:mods="http://www.loc.gov/mods/v3">
+
+    <xsl:output method="xml" indent="yes" encoding="utf-8"/>
+
+    <xsl:param name="pattern"/>
+
+    <xsl:template match="@*|node()">
+        <xsl:copy>
+            <xsl:apply-templates select="@*|node()" />
+        </xsl:copy>
+    </xsl:template>
+
+    <xsl:template match="//mods:titleInfo/mods:title/text()">
+        <xsl:value-of select="replace(., $pattern, '$1')"/>
+    </xsl:template>
+
+    <xsl:template match="//mets:structMap/mets:div/@LABEL">
+        <xsl:attribute name="LABEL">
+            <xsl:value-of select="replace(., $pattern, '$1')"/>
+        </xsl:attribute>
+    </xsl:template>
+
+    <xsl:template match="//mets:structMap/mets:div/@ORDERLABEL">
+        <xsl:attribute name="ORDERLABEL">
+            <xsl:value-of select="replace(., $pattern, '$1')"/>
+        </xsl:attribute>
+    </xsl:template>
+
+</xsl:stylesheet>
