303c793966a75c61153400466bf4144424830ee8
Michael Richter <m.richter@tu-berlin.de>
2019-01-11 18:01:36 +0100

    [#261] PDF/A and metadata

--- kitodo-mediaserver-core/src/main/java/org/kitodo/mediaserver/core/api/IDocument.java
+++ kitodo-mediaserver-core/src/main/java/org/kitodo/mediaserver/core/api/IDocument.java
@@ -11,6 +11,8 @@
 
 package org.kitodo.mediaserver.core.api;
 
+import java.awt.color.ICC_Profile;
+import java.util.Calendar;
 import java.util.List;
 import javax.validation.constraints.NotNull;
 
@@ -30,6 +32,46 @@ public interface IDocument {
      * Get a list of all pages.
      */
     List<IPage> getPages();
+
+    /**
+     * Get document title.
+     */
+    String getTitle();
+
+    /**
+     * Get list of document authors.
+     */
+    List<String> getAuthors();
+
+    /**
+     * Get date of document production.
+     */
+    Calendar getProductionDate();
+
+    /**
+     * Get ICC color profile of document.
+     */
+    ICC_Profile getIccProfile();
+
+    /**
+     * Set the document title.
+     */
+    void setTitle(String title);
+
+    /**
+     * Add an document author.
+     */
+    void addAuthor(String author);
+
+    /**
+     * Set date of document production.
+     */
+    void setProductionDate(Calendar cal);
+
+    /**
+     * Set ICC color profile.
+     */
+    void setIccProfile(ICC_Profile profile);
 
     /**
      * Saves the whole document and its pages to a file.
---
+++ kitodo-mediaserver-core/src/main/java/org/kitodo/mediaserver/core/conversion/PdfboxDocument.java
@@ -0,0 +1,133 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+package org.kitodo.mediaserver.core.conversion;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import org.apache.pdfbox.io.MemoryUsageSetting;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentInformation;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.common.PDMetadata;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
+import org.apache.pdfbox.pdmodel.graphics.color.PDOutputIntent;
+import org.apache.xmpbox.XMPMetadata;
+import org.apache.xmpbox.schema.DublinCoreSchema;
+import org.apache.xmpbox.schema.PDFAIdentificationSchema;
+import org.apache.xmpbox.schema.XMPBasicSchema;
+import org.kitodo.mediaserver.core.api.IPage;
+
+/**
+* A PDF document.
+*/
+public class PdfboxDocument extends AbstractDocument {
+
+    private PDDocument document;
+    private List<IPage> pages = new ArrayList<>();
+
+    public PdfboxDocument(MemoryUsageSetting memoryUsageSetting) {
+        document = new PDDocument(memoryUsageSetting);
+    }
+
+    public PDDocument getDocument() {
+        return document;
+    }
+
+    @Override
+    public List<IPage> getPages() {
+        return pages;
+    }
+
+    @Override
+    public void save(String path) throws Exception {
+
+        // TTF font needed for Unicode support in OCR texts
+        PDFont font = PDType0Font.load(document,
+            PDDocument.class.getResourceAsStream("/org/apache/pdfbox/resources/ttf/LiberationSans-Regular.ttf"), true);
+
+        // Metadata
+        String title = getTitle();
+        List<String> authors = getAuthors();
+        Calendar creationDate = Calendar.getInstance();
+
+        // Create XMP metadata (needed by PDF/A)
+        XMPMetadata xmp = XMPMetadata.createXMPMetadata();
+
+        // Create document information metadata
+        PDDocumentInformation docInfo = new PDDocumentInformation();
+        docInfo.setCreationDate(creationDate);
+
+        DublinCoreSchema dublinCore = xmp.createAndAddDublinCoreSchema();
+
+        if (title != null || !authors.isEmpty()) {
+            if (title != null) {
+                dublinCore.setTitle(title);
+                docInfo.setTitle(title);
+            }
+            authors.forEach(author -> {
+                if (author != null) {
+                    dublinCore.addCreator(author);
+                }
+            });
+            if (dublinCore.getCreators() != null && !dublinCore.getCreators().isEmpty()) {
+                docInfo.setAuthor(String.join(";", authors));
+            }
+        }
+
+        // Add document information metadata
+        document.setDocumentInformation(docInfo);
+
+        // Add XMP basic schema
+        XMPBasicSchema xmpBasic = xmp.createAndAddXMPBasicSchema();
+        xmpBasic.setCreateDate(creationDate);
+
+        // Set PDF/A metadata
+        PDFAIdentificationSchema pdfaId = xmp.createAndAddPFAIdentificationSchema();
+        pdfaId.setPart(1);
+        pdfaId.setConformance("B");
+
+        // Create xpacket text from XMP
+        XmpSerializer serializer = new XmpSerializer();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        serializer.serialize(xmp, outputStream, true);
+
+        // Write XMP metadata to document
+        PDMetadata metadata = new PDMetadata(document);
+        metadata.importXMPMetadata(outputStream.toByteArray());
+        document.getDocumentCatalog().setMetadata(metadata);
+
+        // Set color profile (needed by PDF/A)
+        // Use a default sRGB profile for everything
+        InputStream colorProfileStream = new ByteArrayInputStream(getIccProfile().getData());
+        PDOutputIntent intent = new PDOutputIntent(document, colorProfileStream);
+        intent.setInfo("sRGB");
+        intent.setOutputCondition("sRGB");
+        intent.setOutputConditionIdentifier("sRGB");
+        intent.setRegistryName("");
+        document.getDocumentCatalog().addOutputIntent(intent);
+
+        // Render all pages
+        for (IPage page : pages) {
+            ((PdfboxPage)page).setFont(font);
+            page.renderPage(this);
+            document.addPage((PDPage) page.getPage());
+        }
+
+        document.save(path);
+        document.close();
+    }
+}
